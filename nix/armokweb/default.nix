85dc7a3bbd949f3dda11314771d81d75edb19e93
Morgan Jones <me@numin.it>
2018-07-06 07:10:10 +0000

    Relicense under GPLv3
    AGPL wasn't practical if users needed to modify their code for
    their specific flavor of deployment.

--- nix/armokweb/default.nix
+++ nix/armokweb/default.nix
@@ -1,16 +1,16 @@
 # armokweb: Copyright (C) 2018+ Morgan Jones
 #
 # armokweb is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
+# it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # armokweb is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# GNU General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License
+# You should have received a copy of the GNU General Public License
 # along with armokweb.  If not, see <https://www.gnu.org/licenses/>.
 
 let
@@ -44,7 +44,7 @@ pkgs.stdenv.mkDerivation rec {
 
   meta = with pkgs.stdenv.lib; {
     description = "Play Dwarf Fortress in a browser session";
-    license = licenses.agpl3;
+    license = licenses.gpl3;
     platforms = platforms.linux;
     maintainers = with maintainers; [ numinit ];
   };
